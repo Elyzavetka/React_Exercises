30580380f13985b2f0b9415004e96c990447fbd8
your name <your email>
2023-12-01 23:21:56 +0000

    Add onClick event handler to Button component

---
+++ event_handler_as_a_prop/src/App.js
@@ -0,0 +1,16 @@
+// import './App.css';
+import Button from './components/Button';
+import Talker from './components/Talker';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+      </header>
+      <Talker />
+    </div>
+  );
+}
+
+export default App;
