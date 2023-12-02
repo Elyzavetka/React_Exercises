b811a29e70d27b6b113d6411a70e484fae377dd8
your name <your email>
2023-12-02 18:12:02 +0000

    fixed error

--- event_handler_as_a_prop/src/components/Talker.js
+++ event_handler_as_a_prop/src/components/Talker.js
@@ -5,11 +5,11 @@ function Talker() {
     function talk() {
         let speech = '';
         for (let i = 0; i < 10000; i++) {
-        speech += 'blah ';
+        speech += "blah ";
     }
         alert(speech);
 	}
-    return <Button talk="bar" />;
+    return <Button talk={talk} />;
 }
 
 export default Talker;
